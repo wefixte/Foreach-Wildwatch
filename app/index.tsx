b0dc5816e6b4e961b0e449c95ed0df4f8aecaecc
Cass <cassy.pernelle@gmail.com>
2025-09-25 14:27:28 +0200

    background

--- app/index.tsx
+++ app/index.tsx
@@ -245,7 +245,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    backgroundColor: 'tranparent',
   },
   modalContainer: {
     flex: 1,
